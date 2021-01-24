9ce925df72e8e69e0b8e007ddef7ba8c0ce18521
Pedro Pelizzaro <pedropellizzaro@hotmail.com>
2021-01-24 16:01:21 -0300

    incluindo o componente Link para SPA

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/Link';
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <div>
+      <h1>Início</h1>
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
+    </div>
+  );
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/Link';
+
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <Link href="/">
+        <a>Voltar ao início</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Sobre
